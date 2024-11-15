cd3275908e70478ea921e7bd5696af24726fc33f
Arvind Raj <badam.raj21@st.niituniversity.in>
2024-11-15 16:35:11 +0530

    Changes

--- Search Engine Application/searchengine.py
+++ Search Engine Application/searchengine.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='./static/')
 
 
-# @app.route('/')
-
+@app.route('/')
+def websearch():
+    return render_template("websearch.html")
 
 @app.route('/a')
 def a():
@@ -26,6 +27,15 @@ def d():
 def e():
     return render_template("E.html")
 
+
+
+@app.route('/websearch', methods=['GET', 'POST'])
+def web_search():
+    if request.method == 'POST':
+        query = request.form['query']
+        if query =="":
+            return render_template("websearch.html", query=query)
+        return render_template("results.html", data = query)
 
 
 if __name__ == '__main__':
